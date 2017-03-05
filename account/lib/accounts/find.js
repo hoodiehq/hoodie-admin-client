8b337b48d0c9fe68e8b3f02600f5025c3c80ab45
Gregor <gregor@martynus.net>
2017-03-05 12:47:24 -0800

    fix: add logic from `@hoodie/account-client`

---
+++ account/lib/accounts/find.js
@@ -0,0 +1,31 @@
+module.exports = find
+
+var internals = module.exports.internals = {}
+internals.request = require('../../utils/request')
+internals.deserialise = require('../../utils/deserialise')
+
+function find (state, id, options) {
+  return state.ready
+
+  .then(function () {
+    return internals.request({
+      url: state.url + '/accounts/' + id + query(options),
+      method: 'GET',
+      headers: {
+        authorization: 'Session ' + state.account.session.id
+      }
+    })
+  })
+
+  .then(function (response) {
+    return internals.deserialise(response.body, options)
+  })
+}
+
+function query (options) {
+  if (!options || options.include !== 'profile') {
+    return ''
+  }
+
+  return '?include=profile'
+}
